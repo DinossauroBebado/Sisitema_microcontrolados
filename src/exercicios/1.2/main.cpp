b448c9703ec23c55031c35c4d3c8b7ec287e3d23
DinossauroBebado <guipsilva32@gmail.com>
2025-08-26 16:27:10 -0300

    fix

--- src/exercicios/1.2/main.cpp
+++ src/exercicios/1.2/main.cpp
@@ -4,7 +4,7 @@
 #define TIMER 300
 
 // Array com todos os LEDs
-int leds[] = {LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8, LED_BUILTIN};
+int leds[] = {LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8};
 int num_leds = sizeof(leds) / sizeof(leds[0]);
 
 void setup()
@@ -23,14 +23,14 @@ void setup()
 void loop()
 {
     // Acende todos
-    for (int i = 1; i < num_leds; i++)
+    for (int i = 0; i < num_leds; i++)
     {
         digitalWrite(leds[i], HIGH);
         delay(TIMER);
         digitalWrite(leds[i], LOW);
         delay(TIMER);
     }
-    for (int i = num_leds - 1; i > 0; i--)
+    for (int i = num_leds; i > 0; i--)
     {
         digitalWrite(leds[i], HIGH);
         delay(TIMER);
